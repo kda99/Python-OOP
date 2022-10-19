79671deb7468382d08e42ec732e109157c53ff1e
Dmitry Kapustin <kda99@maik.ru>
2022-10-19 21:30:56 +0300

    Закончил 3.8.7 и добавил тесты

--- 3.8.7.py
+++ 3.8.7.py
@@ -73,15 +73,17 @@ class TicTacToe:  # Игровое поле
         self.verify_index(item)
         r, c = item
         if type(r) == slice:
-            return tuple(self.pole[x][c] for x in range(3))
-        elif type(r) == slice:
-            return tuple(self.pole[r][x] for x in range(3))
+            return tuple(self.pole[x][c].value for x in range(3))
+        elif type(c) == slice:
+            return tuple(self.pole[r][x].value for x in range(3))
+        else:
+            return self.pole[r][c].value
 
     def __setitem__(self, key, value):
         self.verify_index(key)
         r, c = key
         if self.pole[r][c]:
-            self.pole[r][c].value = value.value
+            self.pole[r][c].value = value
             self.pole[r][c].is_free = False
         else:
             raise ValueError('клетка уже занята')
@@ -93,3 +95,39 @@ class Cell:  # Клетка игрового поля
 
     def __bool__(self):  # Возвращает True если клетка свободна, False если нет
         return self.is_free
+
+
+g = TicTacToe()
+g.clear()
+assert g[0, 0] == 0 and g[2, 2] == 0, "начальные значения всех клеток должны быть равны 0"
+g[1, 1] = 1
+g[2, 1] = 2
+assert g[1, 1] == 1 and g[
+    2, 1] == 2, "неверно отработала операция присваивания новых значений клеткам игрового поля (или, некорректно работает считывание значений)"
+
+try:
+    res = g[3, 0]
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError при считывании из несуществующей ячейки"
+
+try:
+    g[3, 0] = 5
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError при записи в несуществующую ячейку"
+
+g.clear()
+g[0, 0] = 1
+g[1, 0] = 2
+g[2, 0] = 3
+
+assert g[0, :] == (1, 0, 0) and g[1, :] == (2, 0, 0) and g[:, 0] == (1, 2, 3), "некорректно отработали срезы после вызова метода clear() и присваивания новых значений"
+
+cell = Cell()
+assert cell.value == 0, "начальное значение атрибута value класса Cell должно быть равно 0"
+res = cell.is_free
+cell.is_free = True
+assert bool(cell), "функция bool вернула False для свободной клетки"
