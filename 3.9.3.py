46f30a10f1a95f673848d1791a8f72e79c937981
Dmitry Kapustin <kda99@maik.ru>
2022-10-20 23:51:57 +0300

    Поправил условие 3.9.3

---
+++ 3.9.3.py
@@ -0,0 +1,55 @@
+"""                                              Условие
+Теперь, вам необходимо разработать итератор, который бы перебирал указанные столбцы двумерного списка. Список представляет собой двумерную таблицу из данных:
+
+lst = [[x11, x12, ..., x1N],
+       [x21, x22, ..., x2N],
+       ...
+       [xM1, xM2, ..., xMN]
+      ]
+Для этого в программе необходимо объявить класс с именем IterColumn, объекты которого создаются командой:
+
+it = IterColumn(lst, column)
+где lst - ссылка на двумерный список; column - индекс перебираемого столбца (отсчитывается от 0).
+
+Затем, с объектами класса IterColumn должны быть доступны следующие операции:
+
+it = IterColumn(lst, 1)
+for x in it:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
+    print(x)
+
+it_iter = iter(it)
+x = next(it_iter)
+P.S. В программе нужно объявить только класс итератора. Выводить на экран ничего не нужно.
+
+"""
+
+class IterColumn:
+    def __init__(self, lst, column):
+        self.lst = lst
+        self.column = column
+        self.start = self.column_yield()
+
+    def column_yield(self):
+        for i in range(len(self.lst)):
+            yield self.lst[i][self.column]
+
+
+    def __next__(self):
+        return next(self.start)
+
+    def __iter__(self):
+        self.start = self.column_yield()
+        return self
+
+
+lst = [['x00', 'x01', 'x02'],
+       ['x10', 'x11', 'x12'],
+       ['x20', 'x21', 'x22'],
+       ['x30', 'x31', 'x32']]
+
+it = IterColumn(lst, 1)
+for x in it:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
+    print(x)
+
+it_iter = iter(it)
+x = next(it_iter)
