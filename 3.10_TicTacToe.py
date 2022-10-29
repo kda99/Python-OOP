1574d0faf15add01860922859a248d48b2ee5fe5
Dmitry Kapustin <kda99@maik.ru>
2022-10-29 13:48:13 +0300

    3.10 Закончил

--- 3.10_TicTacToe.py
+++ 3.10_TicTacToe.py
@@ -1,3 +1,59 @@
+'''
+Необходимо объявить класс с именем TicTacToe (крестики-нолики) для управления игровым процессом. Объекты этого класса
+будут создаваться командой:
+
+game = TicTacToe()
+В каждом объекте этого класса должен быть публичный атрибут:
+
+pole - двумерный кортеж, размером 3x3.
+
+Каждый элемент кортежа pole является объектом класса Cell:
+
+cell = Cell()
+В объектах этого класса должно автоматически формироваться локальное свойство:
+
+value - текущее значение в ячейке: 0 - клетка свободна; 1 - стоит крестик; 2 - стоит нолик.
+
+Также с объектами класса Cell должна выполняться функция:
+
+bool(cell) - возвращает True, если клетка свободна (value = 0) и False - в противном случае.
+
+К каждой клетке игрового поля должен быть доступ через операторы:
+
+res = game[i, j] # получение значения из клетки с индексами i, j
+game[i, j] = value # запись нового значения в клетку с индексами i, j
+Если индексы указаны неверно (не целые числа или числа, выходящие за диапазон [0; 2]), то следует генерировать
+исключение командой:
+
+raise IndexError('некорректно указанные индексы')
+Чтобы в программе не оперировать величинами: 0 - свободная клетка; 1 - крестики и 2 - нолики, в классе TicTacToe должны
+быть три публичных атрибута (атрибуты класса):
+
+FREE_CELL = 0      # свободная клетка
+HUMAN_X = 1        # крестик (игрок - человек)
+COMPUTER_O = 2     # нолик (игрок - компьютер)
+В самом классе TicTacToe должны быть объявлены следующие методы (как минимум):
+
+init() - инициализация игры (очистка игрового поля, возможно, еще какие-либо действия);
+show() - отображение текущего состояния игрового поля (как именно - на свое усмотрение);
+human_go() - реализация хода игрока (запрашивает координаты свободной клетки и ставит туда крестик);
+computer_go() - реализация хода компьютера (ставит случайным образом нолик в свободную клетку).
+
+Также в классе TicTacToe должны быть следующие объекты-свойства (property):
+
+is_human_win - возвращает True, если победил человек, иначе - False;
+is_computer_win - возвращает True, если победил компьютер, иначе - False;
+is_draw - возвращает True, если ничья, иначе - False.
+
+Наконец, с объектами класса TicTacToe должна выполняться функция:
+
+bool(game) - возвращает True, если игра не окончена (никто не победил и есть свободные клетки) и False - в противном
+случае.
+
+Вам в программе необходимо объявить только два класса: TicTacToe и Cell так, чтобы с их помощью можно было бы сыграть в
+"Крестики-нолики" между человеком и компьютером.
+'''
+
 import random
 
 
@@ -98,11 +154,15 @@ class TicTacToe:
         if self.pole[r][c]:
             self.pole[r][c].value = value
             self.pole[r][c].is_free = False
+            self.verify_win(value)
         else:
             raise ValueError('клетка уже занята')
 
     def init(self):
         self.pole = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
+        self.__is_human_win = False
+        self.__is_computer_win = False
+        self.__is_draw = False
 
     def show(self):
         print(' --- --- ---')
@@ -115,7 +175,6 @@ class TicTacToe:
         print('| ' + self.d[self.pole[2][0].value] + ' | ' + self.d[self.pole[2][1].value] + ' | ' + self.d[
             self.pole[2][2].value] + ' |')
         print(' --- --- ---')
-
 
     def human_go(self):
         while True:
@@ -137,14 +196,17 @@ class TicTacToe:
         return not any((self.is_draw, self.is_human_win, self.is_computer_win))
 
 
-
+# Тесты. Закоментировать чтобы играть
 cell = Cell()
 assert cell.value == 0, "начальное значение атрибута value объекта класса Cell должно быть равно 0"
 assert bool(cell), "функция bool для объекта класса Cell вернула неверное значение"
 cell.value = 1
 assert bool(cell) == False, "функция bool для объекта класса Cell вернула неверное значение"
 
-assert hasattr(TicTacToe, 'show') and hasattr(TicTacToe, 'human_go') and hasattr(TicTacToe, 'computer_go'), "класс TicTacToe должен иметь методы show, human_go, computer_go"
+assert hasattr(TicTacToe, 'show') and hasattr(TicTacToe, 'human_go') and hasattr(TicTacToe,
+                                                                                 'computer_go'), "класс TicTacToe должен" \
+                                                                                                 " иметь методы show, " \
+                                                                                                 "human_go, computer_go"
 
 game = TicTacToe()
 assert bool(game), "функция bool вернула неверное значения для объекта класса TicTacToe"
@@ -153,10 +215,12 @@ game[1, 1] = TicTacToe.HUMAN_X
 assert game[1, 1] == TicTacToe.HUMAN_X, "неверно работает оператор присваивания нового значения в ячейку игрового поля"
 
 game[0, 0] = TicTacToe.COMPUTER_O
-assert game[0, 0] == TicTacToe.COMPUTER_O, "неверно работает оператор присваивания нового значения в ячейку игрового поля"
+assert game[
+           0, 0] == TicTacToe.COMPUTER_O, "неверно работает оператор присваивания нового значения в ячейку игрового поля"
 
 game.init()
-assert game[0, 0] == TicTacToe.FREE_CELL and game[1, 1] == TicTacToe.FREE_CELL, "при инициализации игрового поля все клетки должны принимать значение из атрибута FREE_CELL"
+assert game[0, 0] == TicTacToe.FREE_CELL and game[
+    1, 1] == TicTacToe.FREE_CELL, "при инициализации игрового поля все клетки должны принимать значение из атрибута FREE_CELL"
 
 try:
     game[3, 0] = 4
@@ -166,24 +230,39 @@ else:
     assert False, "не сгенерировалось исключение IndexError"
 
 game.init()
-assert game.is_human_win == False and game.is_computer_win == False and game.is_draw == False, "при инициализации игры атрибуты is_human_win, is_computer_win, is_draw должны быть равны False, возможно не пересчитывается статус игры при вызове метода init()"
+assert game.is_human_win == False and game.is_computer_win == False and game.is_draw == False, "при инициализации игры " \
+                                                                                               "атрибуты is_human_win, " \
+                                                                                               "is_computer_win, is_draw" \
+                                                                                               " должны быть равны " \
+                                                                                               "False, возможно не " \
+                                                                                               "пересчитывается статус " \
+                                                                                               "игры при вызове метода " \
+                                                                                               "init()"
 
 game[0, 0] = TicTacToe.HUMAN_X
 game[1, 1] = TicTacToe.HUMAN_X
 game[2, 2] = TicTacToe.HUMAN_X
-assert game.is_human_win and game.is_computer_win == False and game.is_draw == False, "некорректно пересчитываются атрибуты is_human_win, is_computer_win, is_draw. Возможно не пересчитывается статус игры в момент присвоения новых значения по индексам: game[i, j] = value"
+assert game.is_human_win and game.is_computer_win == False and game.is_draw == False, "некорректно пересчитываются " \
+                                                                                      "атрибуты is_human_win, " \
+                                                                                      "is_computer_win, is_draw. " \
+                                                                                      "Возможно не пересчитывается " \
+                                                                                      "статус игры в момент присвоения " \
+                                                                                      "новых значения по индексам: " \
+                                                                                      "game[i, j] = value"
 
 game.init()
 game[0, 0] = TicTacToe.COMPUTER_O
 game[1, 0] = TicTacToe.COMPUTER_O
 game[2, 0] = TicTacToe.COMPUTER_O
-assert game.is_human_win == False and game.is_computer_win and game.is_draw == False, "некорректно пересчитываются атрибуты is_human_win, is_computer_win, is_draw. Возможно не пересчитывается статус игры в момент присвоения новых значения по индексам: game[i, j] = value"
+assert game.is_human_win == False and game.is_computer_win and game.is_draw == False, "некорректно пересчитываются " \
+                                                                                      "атрибуты is_human_win, " \
+                                                                                      "is_computer_win, is_draw. " \
+                                                                                      "Возможно не пересчитывается " \
+                                                                                      "статус игры в момент присвоения " \
+                                                                                      "новых значения по индексам: " \
+                                                                                      "game[i, j] = value"
 
-
-
-
-
-
+# Раскомментировать чтобы играть
 # game = TicTacToe()
 # game.init()
 # step_game = 0
