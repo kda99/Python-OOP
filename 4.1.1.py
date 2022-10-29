78fe944799bc534fcda67049f234623d062163e2
Dmitry Kapustin <kda99@maik.ru>
2022-10-29 15:45:46 +0300

    4.1.1 Закончил

---
+++ 4.1.1.py
@@ -0,0 +1,54 @@
+'''                                                   Условие
+Объявите в программе базовый класс Animal (животное), объекты которого можно создать командой:
+
+an = Animal(name, old)
+где name - название животного (строка); old - возраст животного (целое число). Такие же локальные атрибуты (name и old) должны создаваться в объектах класса.
+
+Далее, объявите дочерний класс (от базового Animal) с именем Cat (кошки), объекты которого создаются командой:
+
+cat = Cat(name, old, color, weight)
+где name, old - те же самые параметры, что и в базовом классе; color - цвет кошки (строка); weight - вес кошки (любое положительное число).
+
+В объектах класса Cat должны автоматически формироваться локальные атрибуты: name, old, color, weight. Формирование атрибутов name, old должен выполнять инициализатор базового класса.
+
+По аналогии объявите еще один дочерний класс Dog (собака), объекты которого создаются командой:
+
+dog = Dog(name, old, breed, size)
+здесь name, old - те же самые параметры, что и в базовом классе; breed - порода собаки (строка); size - кортеж в формате (height, length) высота и длина - числа.
+
+В объектах класса Dog по аналогии должны формироваться локальные атрибуты: name, old, breed, size. За формирование атрибутов name, old отвечает инициализатор базового класса.
+
+Наконец, в классах Cat и Dog объявите метод:
+
+get_info() - для получения информации о животном.
+
+Этот метод должен возвращать строку в формате:
+
+"name: old, <остальные параметры через запятую>"
+'''
+
+
+class Animal:
+    def __init__(self, name: str, old: int):
+        self.name = name
+        self.old = old
+
+
+class Cat(Animal):
+    def __init__(self, name, old, color: str, weight: int):
+        super().__init__(name, old)
+        self.color = color
+        self.weight = weight
+
+    def get_info(self):
+        return f"{self.name}: {self.old}, {self.color}, {self.weight}"
+
+
+class Dog(Animal):
+    def __init__(self, name, old, breed: str, size: int):
+        super().__init__(name, old)
+        self.breed = breed
+        self.size = size
+
+    def get_info(self):
+        return f"{self.name}: {self.old}, {self.breed}, {self.size}"
