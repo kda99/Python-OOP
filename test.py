ed07fe76c8e90dcb65b5bfb7fccb52a229a8a234
Dmitry Kapustin <kda99@maik.ru>
2022-10-29 17:02:57 +0300

    4.1. Закончил

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-t = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
-
-
-
-    print(' --- --- ---')
-    print('| ' + str(table[0][0].value) + ' | ' + str(table[0][1].value) + ' | ' + str(table[0][2].value) + ' |')
-    print(' --- --- ---')
-    print('| ' + str(table[1][0].value) + ' | ' + str(table[1][1].value) + ' | ' + str(table[1][2].value) + ' |')
-    print(' --- --- ---')
-    print('| ' + str(table[2][0].value) + ' | ' + str(table[2][1].value) + ' | ' + str(table[2][2].value) + ' |')
-    print(' --- --- ---')
+a = '4'
+print(a.__doc__)
