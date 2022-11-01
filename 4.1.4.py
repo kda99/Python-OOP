fbde4a9fd2268d88dfcda19a55eee62a986d4bca
kda99 <98463045+kda99@users.noreply.github.com>
2022-11-01 20:19:59 +0300

    Начал 4.1.4

---
+++ 4.1.4.py
@@ -0,0 +1,11 @@
+class Singleton:
+    _instances = {}
+    def __new__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__new__(cls)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+    def __del__(cls):
+        cls.__instance = None
