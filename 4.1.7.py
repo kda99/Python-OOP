761da4e59ef6b2aa6e6b07a1260dfb55aad81fe6
Dmitry Kapustin <kda99@maik.ru>
2022-11-01 22:35:42 +0300

    4.1.7 Закончил

---
+++ 4.1.7.py
@@ -0,0 +1,81 @@
+'''                                    Условия
+Объявите в программе класс Vector, объекты которого создаются командой:
+
+v = Vector(x1, x2, ..., xN)
+где x1, x2, ..., xN - координаты радиус-вектора (числа: целые или вещественные).
+
+С объектами этого класса должны выполняться команды:
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(3, 4, 5)
+v = v1 + v2 # формируется новый вектор (объект класса Vector) с соответствующими координатами
+v = v1 - v2 # формируется новый вектор (объект класса Vector) с соответствующими координатами
+Если размерности векторов v1 и v2 не совпадают, то генерировать исключение:
+
+raise TypeError('размерности векторов не совпадают')
+В самом классе Vector объявите метод с именем get_coords, который возвращает кортеж из текущих координат вектора.
+
+На основе класса Vector объявите дочерний класс VectorInt для работы с целочисленными координатами:
+
+v = VectorInt(1, 2, 3, 4)
+v = VectorInt(1, 0.2, 3, 4) # ошибка: генерируется исключение raise ValueError('координаты должны быть целыми числами')
+При операциях сложения и вычитания с объектом класса VectorInt:
+
+v = v1 + v2  # v1 - объект класса VectorInt
+v = v1 - v2  # v1 - объект класса VectorInt
+должен формироваться объект v как объект класса Vector, если хотя бы одна координата является вещественной. Иначе, v
+должен быть объектом класса VectorInt.
+'''
+
+class Vector:
+    def __init__(self, *args):
+        self.coord = args
+
+    def verify_len(self, other):
+        if len(self.coord) != len(other.coord):
+            raise TypeError('размерности векторов не совпадают')
+
+    def __add__(self, other):
+        self.verify_len(other)
+        return type(other)(*tuple(i + j for i, j in zip(self.coord, other.coord)))
+
+    def __sub__(self, other):
+        self.verify_len(other)
+        return type(self)(*tuple(i - j for i, j in zip(self.coord, other.coord)))
+
+    def get_coords(self):
+        return self.coord
+
+
+class VectorInt(Vector):
+    def __init__(self, *data):
+        if all(map(lambda x: isinstance(x, int), data)):
+            super().__init__(*data)
+        else:
+            raise ValueError('координаты должны быть целыми числами')
+
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(3, 4, 5)
+
+assert (v1 + v2).get_coords() == (4, 6, 8), "операция сложения дала неверные значения (или некорректно работает метод get_coords)"
+assert (v1 - v2).get_coords() == (-2, -2, -2), "операция вычитания дала неверные значения (или некорректно работает метод get_coords)"
+
+v = VectorInt(1, 2, 3, 4)
+assert isinstance(v, Vector), "класс VectorInt должен наследоваться от класса Vector"
+
+try:
+    v = VectorInt(1, 2, 3.4, 4)
+except ValueError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение ValueError для команды v = VectorInt(1, 2, 3.4, 4)"
+
+v1 = VectorInt(1, 2, 3, 4)
+v2 = VectorInt(4, 2, 3, 4)
+v3 = Vector(1.0, 2, 3, 4)
+
+v = v1 + v2
+assert type(v) == VectorInt, "при сложении вектором с целочисленными координатами должен формироваться объект класса VectorInt"
+v = v1 + v3
+assert type(v) == Vector, "при сложении вектором с вещественными координатами должен формироваться объект класса Vector"
